90e786fff6f8efd5d36a21eef4f67ee9ef446084
bstee615 <benjaminjsteenhoek@gmail.com>
2023-01-05 16:49:30 -0600

    Add option for filesystem type

---
+++ MountHelper.ps1
@@ -0,0 +1,27 @@
+﻿param (
+    [Parameter(Mandatory=$true)][string][ValidateSet("mount", "unmount")]$RunMode,
+    [string]$Caption = "Seagate Portable SCSI Disk Device",
+    [int]$Partition = 1,
+    [string]$FilesystemType = "ext4"
+)
+
+$drive= GET-CimInstance -query "SELECT * from Win32_DiskDrive WHERE Caption = '$Caption'"
+"Found drive '$($drive.Caption)' with DeviceID '$($drive.DeviceID)'"
+if (!$RunMode) {
+    "Please provide a mode: [mount, unmount]"
+    exit -1
+}
+if ($RunMode -eq "mount") {
+    "Mounting..."
+    wsl --mount $drive.DeviceID --partition $Partition --type $FilesystemType
+    exit $?
+}
+elseif ($RunMode -eq "unmount") {
+    "Unmounting..."
+    wsl --unmount $drive.DeviceID
+    exit $?
+}
+else {
+    "Please provide a mode: [mount, unmount]"
+    exit -1
+}
